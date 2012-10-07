2e6af803554dd8f9b489012e0f9daf64d14201f6
Jiew Meng <jiewmeng@gmail.com>
2012-10-07 21:23:21 +0800

    l1 line size: now using median

--- cache-l1-line.cpp
+++ cache-l1-line.cpp
@@ -1,20 +1,25 @@
 #include <stdio.h> 
 #include <time.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 #define KB 1024 // 1 KB is 1024 bytes
 #define MB 1024 * KB // 1 MB is 1024 KB
-#define SIZE 4 * MB // Size of the data array
-#define REPS 256 * MB // times to access/modify memory (MB/KB just used as millions/thousands multiplier)
+#define SIZE 6 * MB // Size of the data array
+#define REPS 1024 * MB // times to access/modify memory (MB/KB just used as millions/thousands multiplier)
 #define MAX_STRIDE 512 // in bytes, should be multiple of 4 (sizeof(int))
-#define TIMES 6 // times to run to get average
+#define TIMES 12 // times to run to get median
 
 long long wall_clock_time();
  
 int main() {
+	using namespace std;
 	long long start, end;
 	int lengthMod;
 	float totalTime;
-	int tmp = 0;
+	int tmp = 0, median, medianIndex = TIMES / 2;
+	vector<float> times(TIMES, 1.0000); 
 
 	int *data = new int[SIZE/sizeof(int)]; 
 	lengthMod = SIZE/sizeof(int) - 1;
@@ -29,10 +34,11 @@ int main() {
 				tmp += data[(k * i) & lengthMod];
 			}
 			end = wall_clock_time();
-			totalTime += ((float)(end - start))/1000000000;
+			times[j] = ((float)(end - start))/1000000000;
 		}
 		// we can expect a spike in time (more cache miss) when it exceeds the line size
-		printf("%d, %1.2f \n", (int)(i * sizeof(int)), totalTime/TIMES);
+		nth_element(times.begin(), times.begin() + medianIndex, times.end());
+		printf("%d, %1.2f \n", (int)(i * sizeof(int)), times[medianIndex]);
 	}
 
 	// ensure tmp is used to prevent over-optimization
